d3f438f2923155715b8c0357d441836117a07f72
ZEP <zephyr.lathuy.zpl@gmail.com>
2025-02-19 10:36:04 +0100

    tout remis en place sur le main

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+// js pour login style
+const container = document.querySelector(".container");
+const registerBtn = document.querySelector(".register-btn");
+const loginBtn = document.querySelector(".login-btn");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
