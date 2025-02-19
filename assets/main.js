f7e15dcea83705ccb45d6a697670fc2ecd7d07a1
ZEP <zephyr.lathuy.zpl@gmail.com>
2025-02-19 13:00:01 +0100

    change on style

--- assets/main.js
+++ assets/main.js
@@ -10,3 +10,19 @@ registerBtn.addEventListener("click", () => {
 loginBtn.addEventListener("click", () => {
   container.classList.remove("active");
 });
+
+function togglePassword(passwordId, iconId) {
+  var passwordField = document.getElementById(passwordId);
+  var showPasswordIcon = document.getElementById(iconId);
+
+  // Si le type est "password", changer en "text", sinon en "password"
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    showPasswordIcon.classList.remove("bxs-lock-alt");
+    showPasswordIcon.classList.add("bxs-lock-open-alt");
+  } else {
+    passwordField.type = "password";
+    showPasswordIcon.classList.remove("bxs-lock-open-alt");
+    showPasswordIcon.classList.add("bxs-lock-alt");
+  }
+}
